505f27c27b1819fab2acd8b484510610230cff3e
R0me0Star <eip_spb@mail.ru>
2026-09-24 19:10:25 +0300

    data on stream; memory/basic errors check; main finished

--- pozdnyakov.egor/N1/main.cpp
+++ pozdnyakov.egor/N1/main.cpp
@@ -2,32 +2,53 @@
 #include <iostream>
 #include <istream>
 #include <new>
+#include "arguments.hpp"
+#include "array.hpp"
+#include "transaction-io.hpp"
+#include "transaction.hpp"
 
-int main()
+int main(int argc, char ** argv)
 {
   constexpr int invalid_arguments_code = 1;
   constexpr int file_error_code = 2;
-  //pozdnyakov::Arguments arguments{"", "", false, false};
+  pozdnyakov::Arguments arguments{"", "", false, false};
   if (!pozdnyakov::parseArguments(argc, argv, arguments)) {
     std::cerr << "Invalid command line arguments\n";
-    return somethingArguments;
+    return invalid_arguments_code;
   }
 
+  pozdnyakov::Array< pozdnyakov::Transaction > transactions{nullptr, 0, 0};
+  pozdnyakov::ReadStatistics statistics{0, 0};
   std::ifstream file;
   if (arguments.has_input) {
     file.open(arguments.input);
     if (!file.is_open()) {
       std::cerr << "Cannot open input file\n";
-      return something;
+      return file_error_code;
     }
   }
   std::istream & input = arguments.has_input ? file : std::cin;
   try {
-    statistics = pozdnyakov::readTransactions(input, );
+    statistics = pozdnyakov::readTransactions(input, transactions);
   } catch (const std::bad_alloc & error) {
     pozdnyakov::clear(transactions);
     std::cerr << "Not enough memory: " << error.what() << '\n';
-    return something;
+    return file_error_code;
   }
+  std::cerr << statistics.accepted << ' ' << statistics.ignored << '\n';
 
+  int code = 0;
+  if (arguments.has_output) {
+    std::ofstream output(arguments.output);
+    if (output.is_open()) {
+      pozdnyakov::writeTransactions(output, transactions);
+    } else {
+      std::cerr << "Cannot open output file\n";
+      code = file_error_code;
+    }
+  } else {
+    pozdnyakov::writeTransactions(std::cout, transactions);
+  }
+  pozdnyakov::clear(transactions);
+  return code;
 }
