0c32c36baf8ab95ff04dbb58732b38267d073fc5
R0me0Star <eip_spb@mail.ru>
2026-09-24 19:14:52 +0300

    try check; funcs in array; constexpr instead of const

--- pozdnyakov.egor/N1/array.hpp
+++ pozdnyakov.egor/N1/array.hpp
@@ -13,7 +13,30 @@ namespace pozdnyakov {
 
   template< class T >
   void pushBack(Array< T > & array, const T & value)
-  {}
+  {
+    if (array.size < array.capacity) {
+      array.data[array.size] = value;
+      ++array.size;
+      return;
+    }
+    constexpr std::size_t initial_capacity = 16;
+    constexpr std::size_t growth_factor = 2;
+    const std::size_t new_capacity = array.capacity == 0 ? initial_capacity : array.capacity * growth_factor;
+    T * const new_data = new T[new_capacity];
+    try {
+      for (std::size_t i = 0; i < array.size; ++i) {
+        new_data[i] = array.data[i];
+      }
+      new_data[array.size] = value;
+    } catch (...) {
+      delete[] new_data;
+      throw;
+    }
+    delete[] array.data;
+    array.data = new_data;
+    array.capacity = new_capacity;
+    ++array.size;
+  }
 
   template< class T >
   void clear(Array< T > & array) noexcept
