19fc5cccdc3727c2f64481acad9f09d0ea863475
R0me0Star <eip_spb@mail.ru>
2026-09-24 20:20:16 +0300

    code 0 for empty input; no error

--- pozdnyakov.egor/N1/main.cpp
+++ pozdnyakov.egor/N1/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char ** argv)
 {
   constexpr int max_arguments = 2;
   constexpr int invalid_arguments_code = 1;
-  constexpr int empty_input_code = 1;
+  constexpr int empty_input_code = 0;
   constexpr int file_error_code = 2;
   if (argc - 1 > max_arguments) {
     std::cerr << "Too many command line arguments\n";
@@ -43,7 +43,6 @@ int main(int argc, char ** argv)
   }
   if (statistics.accepted == 0 && statistics.ignored == 0) {
     std::cout << '\n';
-    std::cerr << "Empty input\n";
     return empty_input_code;
   }
   std::cerr << statistics.accepted << ' ' << statistics.ignored << '\n';
